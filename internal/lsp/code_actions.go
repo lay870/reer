e4a5ba09373f1341b6fda6299b6cba8463d5da72
James Pogran <jpogran@outlook.com>
2021-10-18 08:39:28 -0400

    Fix Code Action Resolution
    This fixes several parts of the code action handler workflow.
    
    - Remove `source.fixAll` because the action implies fixing errors, but terraform fmt only formats code style.
    - Remove `source` as it is a base type and not a specific action.
    - Correctly set the code action value in the returned `lsp.CodeAction` response to the one chosen by the user.
    - Exit early so as to not format a document without an explicit request. When additional code actions are added, we can modify this to return a merged list of actions to apply.

--- internal/lsp/code_actions.go
+++ internal/lsp/code_actions.go
@@ -15,9 +15,24 @@ type CodeActions map[lsp.CodeActionKind]bool
 
 var (
 	SupportedCodeActions = CodeActions{
-		lsp.Source:                 true,
-		lsp.SourceFixAll:           true,
-		SourceFormatAll:            true,
+		// `source.*`: Source code actions apply to the entire file. They must be explicitly
+		// requested and will not show in the normal lightbulb menu. Source actions
+		// can be run on save using editor.codeActionsOnSave and are also shown in
+		// the source context menu.
+		// For action definitions, refer to: https://code.visualstudio.com/api/references/vscode-api#CodeActionKind
+
+		// `source.fixAll`: Fix all actions automatically fix errors that have a clear fix that do
+		// not require user input. They should not suppress errors or perform unsafe
+		// fixes such as generating new types or classes.
+		// ** We don't support this as terraform fmt only adjusts style**
+		// lsp.SourceFixAll: true,
+
+		// `source.formatAll`: Generic format code action. We register this as
+		// terraform fmt can apply to entire documents or ranges
+		SourceFormatAll: true,
+
+		// `source.formatAll.terraform-ls`: Terraform specific format code action.
+		// We register so that users can choose to allow only terraform type formatting
 		SourceFormatAllTerraformLs: true,
 	}
 )
@@ -35,13 +50,8 @@ func (c CodeActions) AsSlice() []lsp.CodeActionKind {
 }
 
 func (ca CodeActions) Only(only []lsp.CodeActionKind) CodeActions {
-	// if only is empty, assume that the client wants all code actions
-	// else build mapping of requested and determine if supported
-	if len(only) == 0 {
-		return ca
-	}
-
 	wanted := make(CodeActions, 0)
+
 	for _, kind := range only {
 		if v, ok := ca[kind]; ok {
 			wanted[kind] = v
